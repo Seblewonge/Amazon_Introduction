2e921122b2343b63c4baf73daede53f15c3fc5d2
seblewonge <seblewongelrcd@gmail.com>
2025-09-27 22:37:33 +0300

    last commit

--- amazone-clone/src/API/axios.js
+++ amazone-clone/src/API/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-6f969/us-central1/api",
+//local instance of firebase function
+  //baseURL: "http://127.0.0.1:5001/clone-6f969/us-central1/api",
+//deployed version of amazon server on render.con
+  baseURL: "https://amazon-api-deploy-rwb4.onrender.com/",
 });
 
 export{axiosInstance}
